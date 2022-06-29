c557bc9eb5c0518d22a7628370e42caacaced327
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-06-29 15:42:00 +0300

    Update 01. Secret Chat.js

--- Fundamentals/Exam Preparation 1.js/01. Secret Chat.js
+++ Fundamentals/Exam Preparation 1.js/01. Secret Chat.js
@@ -1,41 +1,40 @@
 function secretChat(input){
     
-    let message = input.shift()
+    let message = input.shift();                             // Изваждаме съобщението от инпута 
 
-    while (input[0] !== 'Reveal'){
-        let line = input.shift()
-        line = line.split(":|:")
-        let command = line[0]
-        if (command === 'InsertSpace'){
-            let index = line[1]
-            let firstHalf = message.substring(0, index)
-            let secondHalf = message.substring(index)
-            message = firstHalf + ' ' + secondHalf
-            console.log(message)
-        } else if (command === 'Reverse'){
-            let substring = line[1]
-            if (!message.includes(substring)){
-                console.log("error")
+    while (input[0] !== 'Reveal'){                           // Ако елемента на нулев индекс, не ни е Reveal въртим цикъла
+        let line = input.shift().split(":|:");               // Взимаме линията от масива, разделена по :|:
+        let command = line[0]         ;                      // Самата команда е на нулев индекс 
+
+        if (command === 'InsertSpace'){                      // Ако командата е InsertSpace 
+            let index = line[1];                             // Намираме индекса на първи индекс от линията 
+            let firstHalf = message.substring(0, index);     // Първата половина е от 0 до индекса 
+            let secondHalf = message.substring(index);       // Втората половина е от индекса до края на съобщението
+            message = firstHalf + ' ' + secondHalf;          // Променяме съобщението, като събираме първата половина + интервал + втората половина 
+            console.log(message);                            // Печатаме съобщението 
+
+        } else if (command === 'Reverse'){                   // Ако командата е InsertSpace
+            let substring = line[1];                         // Намираме подстринга на първи индекс от линията 
+            if (!message.includes(substring)){               // Ако в съобщението НЯМА подстринга
+                console.log("error");                        // Печатаме грешка 
             } else {
-                let index = message.indexOf(substring)
-                message = message.split("")
-                let cutted = message.splice(index, substring.length).reverse()
-                for (let el of cutted){
-                    message.push(el)
-                }
-                message =  message.join("")
-                console.log(message)
+                let index = message.indexOf(substring);      // Намираме индекса, на който започва подстринга 
+                let firstHalf = message.substring(0, index); // Първата половина е от 0 до индекса 
+                let secondHalf = message.substring(index + substring.length);    // Втората половина започва от индекса + дължината на подстринга до края на съобщението
+                message = firstHalf + secondHalf + substring.split("").reverse().join("");
+                // Променяме съобщението, като събираме първата и втората половина и към тям добавяме обърнатия подстринг   
+                console.log(message);                        // Печатаме съобщението
             }
-        } else if (command === 'ChangeAll'){
-            let occurences = line[1]
-            let replacement = line[2]
-            for (let letter of message){
-                message = message.replace(occurences, replacement)
-            }
-            console.log(message)
+
+        } else if (command === 'ChangeAll'){                 // Ако командата е InsertSpace
+            let occurences = line[1];                        // Буквите, които трябва да променим
+            let replacement = line[2];                       // Буквите, с които трябва да заменим 
+            message = message.split(occurences).join(replacement);   // Променяме съобщението, като сплитваме по срещанията и джоинваме по заменката 
+            console.log(message);                            // Печатаме съобщението
         }
+
     }
-    console.log(`You have a new text message: ${message}`)
+    console.log(`You have a new text message: ${message}`);  // Печатаме съобщението накрая 
 
 }
 secretChat([
