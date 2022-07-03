1dcf6d637bc0e3e070834f23cb55890773c48968
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-07-03 13:01:17 +0300

    Create 09. Square of Stars.js

---
+++ JS Advanced/09. Square of Stars.js
@@ -0,0 +1,11 @@
+    function squareOfStart(num){
+
+        for (let i = 0; i < num;i++){
+            let output = ''
+            for (let i = 0; i < num;i++){
+                output += '*' + ' '
+            }
+            console.log(output)
+        }
+    }
+squareOfStart(7)
