654b3fa8b4f559ff1d0fd5f1dd11d0df367f77d2
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-07-04 12:49:34 +0300

    Create 06. Sum of Numbers N…M.js

---
+++ JS Advanced/Syntax, Functions and Statements - Lab/06. Sum of Numbers N…M.js
@@ -0,0 +1,13 @@
+function sumOfNumbersNM(n, m) {
+    
+    n = Number(n);
+    m = Number(m);
+    let sum = 0;
+
+    for (let i = n; i <= m; i++) {
+        sum += i;
+    }
+    return sum;
+}
+sumOfNumbersNM("1", "5");
+sumOfNumbersNM("-8", "20");
