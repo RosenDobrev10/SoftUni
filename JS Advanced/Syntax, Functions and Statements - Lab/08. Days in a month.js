4e49191c09d2843eea39f3fc72f04eb94b6ec7e2
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-07-04 12:50:01 +0300

    Create 08. Days in a month.js

---
+++ JS Advanced/Syntax, Functions and Statements - Lab/08. Days in a month.js
@@ -0,0 +1,7 @@
+function daysInAMonth(month, year) {
+    
+    let date = new Date(year, month, 0).toDateString();
+    return date.substring(8, 10);
+}
+daysInAMonth(1, 2012);
+daysInAMonth(2, 2021);
