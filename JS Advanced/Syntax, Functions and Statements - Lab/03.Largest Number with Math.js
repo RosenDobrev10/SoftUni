250b480ac595746a6d1cee95d8399eaf7444eb4d
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-07-04 12:48:44 +0300

    Create 03.Largest Number with Math.js

---
+++ JS Advanced/Syntax, Functions and Statements - Lab/03.Largest Number with Math.js
@@ -0,0 +1,7 @@
+function largestNumber(numOne, numTwo, numThree) {
+    
+    let max = Math.max(numOne, numTwo, numThree)
+    console.log(`The largest number is ${max}.`);
+}
+largestNumber(5, -3, 16);
+largestNumber(-3, -5, -22.5);
