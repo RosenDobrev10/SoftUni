475b67d0e4a7979790e42556b6951ab444403282
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-07-04 12:47:29 +0300

    change

--- JS Advanced/01. Echo Function.js
+++
@@ -1,8 +0,0 @@
-function echoFunction(string){
-
-    console.log(string.length)
-    console.log(string)
-
-}
-echoFunction('Hello, JavaScript!')
-echoFunction('strings are easy')
--- JS Advanced/08. Days in a month.js
+++
@@ -1,7 +0,0 @@
-function daysInAMonth(month, year){
-
-    let date = new Date(year, month, 0).toDateString()
-    return date.substring(8, 10)
-}
-daysInAMonth(1, 2012)
-daysInAMonth(2, 2021)
