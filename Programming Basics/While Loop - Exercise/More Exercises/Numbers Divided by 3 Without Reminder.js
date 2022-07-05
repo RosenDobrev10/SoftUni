1a51171e84ddbfe07a8a4216f2885fe434931615
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-07-05 18:23:43 +0300

    delete

--- Programming Basics/While Loop - Exercise/More Exercises/Numbers Divided by 3 Without Reminder.js
+++
@@ -1,8 +0,0 @@
-function numbersDividedby3WithoutReminder(input) {
-    for (let i = 1; i <= 100; i++) {
-        if (i % 3 === 0) {
-            console.log(i);
-        }
-    }
-}
-numbersDividedby3WithoutReminder();
