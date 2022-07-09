02f8820b96ef95cc1a752a017d7dfea0012f788e
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-07-09 12:43:00 +0300

    Create 01. City Record.js

---
+++ JS Advanced/03. Objects and Composition - Lab/01. City Record.js
@@ -0,0 +1,11 @@
+function cityRecord(name, population, treasury) {
+    
+    const city = {
+        name,
+        population,
+        treasury,
+    };
+    return city;
+}
+cityRecord("Tortuga", 7000, 15000);
+cityRecord("Santo Domingo", 12000, 23500);
