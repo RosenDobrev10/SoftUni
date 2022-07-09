35e32a90b22af88e5c14b8d46417d2795024bec2
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-07-09 12:43:12 +0300

    Create 02. Town Population.js

---
+++ JS Advanced/03. Objects and Composition - Lab/02. Town Population.js
@@ -0,0 +1,29 @@
+function townPopulation(array) {
+
+    const towns = {};
+
+    for (const line of array) {
+        const [town, population] = line.split(" <-> ");
+        if (!towns.hasOwnProperty(town)) {
+            towns[town] = 0;
+        }
+        towns[town] += Number(population);
+    }
+
+    Object.keys(towns).forEach((town) => console.log(`${town} : ${towns[town]}`));
+}
+townPopulation([
+    "Sofia <-> 1200000",
+    "Montana <-> 20000",
+    "New York <-> 10000000",
+    "Washington <-> 2345000",
+    "Las Vegas <-> 1000000",
+]);
+
+townPopulation([
+    "Istanbul <-> 100000",
+    "Honk Kong <-> 2100004",
+    "Jerusalem <-> 2352344",
+    "Mexico City <-> 23401925",
+    "Istanbul <-> 1000",
+]);
