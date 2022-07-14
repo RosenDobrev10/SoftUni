087a4d7ddd1cfc456bc0f5300b58372040d275e2
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-07-14 10:55:14 +0300

    Update 02. Student Information.js

--- Fundamentals/01. Basic Syntax, Conditional Statements and Loops - Lab/02. Student Information.js
+++ Fundamentals/01. Basic Syntax, Conditional Statements and Loops - Lab/02. Student Information.js
@@ -1,4 +1,6 @@
 function studentInformation(name, age, averageGrade) {
+    
     console.log(`Name: ${name}, Age: ${age}, Grade: ${averageGrade.toFixed(2)}`);
+    
 }
 studentInformation("John", 15, 5.54678);
