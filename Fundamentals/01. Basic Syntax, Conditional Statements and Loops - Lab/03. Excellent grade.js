14b70242b6870bbd9c1106aefeb7a63673715fac
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-07-14 10:56:39 +0300

    Update 03. Excellent grade.js

--- Fundamentals/01. Basic Syntax, Conditional Statements and Loops - Lab/03. Excellent grade.js
+++ Fundamentals/01. Basic Syntax, Conditional Statements and Loops - Lab/03. Excellent grade.js
@@ -1,9 +1,7 @@
 function excellentGrade(grade) {
-    if (grade >= 5.5) {
-        console.log("Excellent");
-    } else {
-        console.log("Not excellent");
-    }
+    
+   grade >= 5.5 ? console.log("Excellent") : console.log("Not excellent");
+
 }
 excellentGrade(5.5);
 excellentGrade(4.35);
