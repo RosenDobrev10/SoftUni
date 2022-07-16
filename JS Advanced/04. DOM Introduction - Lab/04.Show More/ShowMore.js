215b1a55a5825064f4f02240c3d884f468d547d1
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-07-16 10:29:23 +0300

    Create ShowMore.js

---
+++ JS Advanced/04. DOM Introduction - Lab/04.Show More/ShowMore.js
@@ -0,0 +1,5 @@
+function showText() {
+
+    document.getElementById("text").style.display = "inline";    // Показваме текста с id text 
+    document.getElementById("more").style.display = "none";     // Скриваме бутона с id more 
+}
