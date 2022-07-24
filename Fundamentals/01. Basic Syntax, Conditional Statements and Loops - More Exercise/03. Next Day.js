bfb36595a8cfe5d969f53f9f475ce6abc77c821e
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-07-24 19:05:19 +0300

    Update 03. Next Day.js

--- Fundamentals/01. Basic Syntax, Conditional Statements and Loops - More Exercise/03. Next Day.js
+++ Fundamentals/01. Basic Syntax, Conditional Statements and Loops - More Exercise/03. Next Day.js
@@ -1,8 +1,10 @@
 function nextDay(year, month, day) {
+    
     let nextDay = new Date(year, month - 1, day + 1);
     let newYear = nextDay.getFullYear();
     let newMonth = nextDay.getMonth() + 1;
     let newDate = nextDay.getDate();
+    
     console.log(`${newYear}-${newMonth}-${newDate}`);
 }
 nextDay(2016, 9, 30);
