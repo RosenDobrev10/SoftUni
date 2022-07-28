8eea20a676da199d4bb2882ebf27add0b9463d14
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-07-28 18:17:46 +0300

    Update 02. Concatenate Names.js

--- Fundamentals/02. Data Types and Variables - Lab/02. Concatenate Names.js
+++ Fundamentals/02. Data Types and Variables - Lab/02. Concatenate Names.js
@@ -1,4 +1,5 @@
 function concatenateNames(firstName, lastName, delimeter) {
+    
     console.log(firstName + delimeter + lastName); // Конкатениране на стрингове чрез +
     console.log(`${firstName}${delimeter}${lastName}`); // Интерполация на стрингове чрез ``
 }
