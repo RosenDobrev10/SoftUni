eeb2607cd4846d1e8ebd4cbb67635c5a3ac9ab33
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-07-30 17:42:35 +0300

    Update 04. Integer and Float.js

--- Fundamentals/02. Data Types and Variables - Lab/04. Integer and Float.js
+++ Fundamentals/02. Data Types and Variables - Lab/04. Integer and Float.js
@@ -1,4 +1,5 @@
 function integerAndFloat(num1, num2, num3) {
+    
     let sum = num1 + num2 + num3;
     let sumToString = String(sum) // String(num) и num.toString() - Преобразуват число в стринг 
 
