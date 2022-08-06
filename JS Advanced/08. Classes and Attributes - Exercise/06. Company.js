be6d6eb1ab8f92babf7cce37251bc78f8f9adc57
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-08-06 13:25:09 +0300

    Create 06. Company.js

---
+++ JS Advanced/08. Classes and Attributes - Exercise/06. Company.js
@@ -0,0 +1,71 @@
+class Company {
+	constructor () {
+		this.departments = []
+	}
+
+	getDepart (name) {
+		const depart = this.departments.find(x => x.name === name)
+
+		if (! depart) {
+			const temp = { name, employees: [], salaries: [], positions: [] }
+			this.departments.push(temp)
+			return temp
+		} else {
+			return depart
+		}
+	}
+
+	getSalariesSum = depart => depart.salaries.reduce((a, v) => a + v, 0)
+
+	bestSalaryDepart = () =>
+		this.departments.sort((a, b) =>
+			this.getSalariesSum(b) / b.salaries.length -
+			this.getSalariesSum(a) / a.salaries.length)
+			[0]
+
+	addEmployee (...args) {
+		if (
+			args.some(x => x === undefined || x === null || x === '') ||
+			args[1] < 0
+		) {
+			throw new Error('Invalid input!')
+		}
+
+		const department = this.getDepart(args[3])
+
+		department.employees.push(args[0])
+		department.salaries.push(args[1])
+		department.positions.push(args[2])
+		return `New employee is hired. Name: ${args[0]}. Position: ${args[2]}`
+	}
+
+	bestDepartment () {
+		const best = this.bestSalaryDepart()
+
+		const printData = best.employees
+			.reduce((a, v, i) => {
+				a[i] = []
+				a[i].push(v, best.salaries[i], best.positions[i])
+				return a
+			}, [])
+			.sort((a, b) => b[1] - a[1] === 0
+				? a[0].localeCompare(b[0])
+				: b[1] - a[1])
+			.map(x => x.join(' '))
+			.join('\n')
+
+		return `Best Department is: ${best.name}
+Average salary: ${(this.getSalariesSum(best) / best.salaries.length).toFixed(2)}
+${printData}`
+	}
+}
+
+let c = new Company();
+c.addEmployee("Stanimir", 2000, "engineer", "Construction");
+c.addEmployee("Pesho", 1500, "electrical engineer", "Construction");
+c.addEmployee("Slavi", 500, "dyer", "Construction");
+c.addEmployee("Stan", 2000, "architect", "Construction");
+c.addEmployee("Stanimir", 1200, "digital marketing manager", "Marketing");
+c.addEmployee("Pesho", 1000, "graphical designer", "Marketing");
+c.addEmployee("Gosho", 1350, "HR", "Human resources");
+console.log(c.bestDepartment());
