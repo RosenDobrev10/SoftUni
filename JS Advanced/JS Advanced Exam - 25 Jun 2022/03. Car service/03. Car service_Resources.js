6d505cd3c047d7fd85bc3e61d4a9d14b5cb754fa
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-08-10 15:30:12 +0300

    Create 03. Car service_Resources.js

---
+++ JS Advanced/JS Advanced Exam - 25 Jun 2022/03. Car service/03. Car service_Resources.js
@@ -0,0 +1,51 @@
+const carService = {
+
+  isItExpensive(issue) {
+    if (issue === "Engine" || issue === "Transmission") {
+      return `The issue with the car is more severe and it will cost more money`;
+    } else {
+      return `The overall price will be a bit cheaper`;
+    }
+  },
+
+  discount(numberOfParts, totalPrice) {
+    if (typeof numberOfParts !== "number" || typeof totalPrice !== "number") {
+      throw new Error("Invalid input");
+    }
+
+    let discountPercentage = 0;
+
+    if (numberOfParts > 2 && numberOfParts <= 7) {
+      discountPercentage = 15;
+    } else if (numberOfParts > 7) {
+      discountPercentage = 30;
+    }
+
+    let result = (discountPercentage / 100) * totalPrice;
+
+    if (numberOfParts <= 2) {
+      return "You cannot apply a discount";
+    } else {
+      return `Discount applied! You saved ${result}$`;
+    }
+  },
+
+  partsToBuy(partsCatalog, neededParts) {
+    let totalSum = 0;
+
+    if (!Array.isArray(partsCatalog) || !Array.isArray(neededParts)) {
+      throw new Error("Invalid input");
+    }
+    neededParts.forEach((neededPart) => {
+      partsCatalog.map((obj) => {
+        if (obj.part === neededPart) {
+          totalSum += obj.price;
+        }
+      });
+    });
+
+    return totalSum;
+  },
+};
+
+module.exports = { carService }
