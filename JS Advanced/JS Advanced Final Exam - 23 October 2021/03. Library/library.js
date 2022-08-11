71f858d654d2e13af7e520fb72fa95c982bbb378
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-08-11 10:44:03 +0300

    Create library.js

---
+++ JS Advanced/JS Advanced Final Exam - 23 October 2021/03. Library/library.js
@@ -0,0 +1,39 @@
+const library = {
+
+    calcPriceOfBook(nameOfBook, year) {
+        let price = 20;
+        if (typeof nameOfBook != "string" || !Number.isInteger(year)) {
+            throw new Error("Invalid input");
+        } else if (year <= 1980) {
+            let total = price - (price * 0.5);
+            return `Price of ${nameOfBook} is ${total.toFixed(2)}`;
+        }
+        return `Price of ${nameOfBook} is ${price.toFixed(2)}`;
+    },
+
+    findBook: function(booksArr, desiredBook) {
+        if (booksArr.length == 0) {
+            throw new Error("No books currently available");
+        } else if (booksArr.find(e => e == desiredBook)) {
+            return "We found the book you want.";
+        } else {
+            return "The book you are looking for is not here!";
+        }
+
+    },
+
+    arrangeTheBooks(countBooks) {
+        const countShelves = 5;
+        const availableSpace = countShelves * 8;
+
+        if (!Number.isInteger(countBooks) || countBooks < 0) {
+            throw new Error("Invalid input");
+        } else if (availableSpace >= countBooks) {
+            return "Great job, the books are arranged.";
+        } else {
+            return "Insufficient space, more shelves need to be purchased.";
+        }
+    }
+
+};
+module.exports = { library }
