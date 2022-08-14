495a65ef4e74c7920a48d0110923881e1ab5f8e2
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-08-14 15:02:24 +0300

    Create 03. Number Operations.js

---
+++ JS Advanced/JS Advanced Exam - 20 February 2021/03. Number Operations/03. Number Operations.js
@@ -0,0 +1,37 @@
+const numberOperations = {
+    
+    powNumber: function (num) {
+        return num * num;
+    },
+
+    numberChecker: function (input) {
+        input = Number(input);
+
+        if (isNaN(input)) {
+            throw new Error('The input is not a number!');
+        }
+
+        if (input < 100) {
+            return 'The number is lower than 100!';
+        } else {
+            return 'The number is greater or equal to 100!';
+        }
+    },
+
+    sumArrays: function (array1, array2) {
+
+        const longerArr = array1.length > array2.length ? array1 : array2;
+        const rounds = array1.length < array2.length ? array1.length : array2.length;
+
+        const resultArr = [];
+
+        for (let i = 0; i < rounds; i++) {
+            resultArr.push(array1[i] + array2[i]);
+        }
+
+        resultArr.push(...longerArr.slice(rounds));
+
+        return resultArr
+    }
+};
+module.exports = { numberOperations }
