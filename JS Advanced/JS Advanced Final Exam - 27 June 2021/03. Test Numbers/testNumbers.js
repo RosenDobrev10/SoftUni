07befbb5e02814955947a7962313993c3b18fcfb
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-08-16 12:29:37 +0300

    Create testNumbers.js

---
+++ JS Advanced/JS Advanced Final Exam - 27 June 2021/03. Test Numbers/testNumbers.js
@@ -0,0 +1,40 @@
+const testNumbers = {
+    
+    sumNumbers: function (num1, num2) {
+        let sum = 0;
+
+        if (typeof(num1) !== 'number' || typeof(num2) !== 'number') {
+            return undefined;
+        } else {
+            sum = (num1 + num2).toFixed(2);
+            return sum
+        }
+    },
+
+    numberChecker: function (input) {
+        input = Number(input);
+
+        if (isNaN(input)) {
+            throw new Error('The input is not a number!');
+        }
+
+        if (input % 2 === 0) {
+            return 'The number is even!';
+        } else {
+            return 'The number is odd!';
+        }
+
+    },
+
+    averageSumArray: function (arr) {
+
+        let arraySum = 0;
+
+        for (let i = 0; i < arr.length; i++) {
+            arraySum += arr[i]
+        }
+
+        return arraySum / arr.length
+    }
+};
+module.exports = { testNumbers }
