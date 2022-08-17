cfc8302d797bbb0d9a5d1e0d921ed9f998436702
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-08-17 17:37:25 +0300

    Create solution.js

---
+++ JS Advanced/JS Advanced Retake Exam - 6 Apr 2022/03. Book-selection/solution.js
@@ -0,0 +1,39 @@
+const bookSelection = {
+  isGenreSuitable(genre, age) {
+    if (age <= 12 && (genre === "Thriller" || genre === "Horror")) {
+      return `Books with ${genre} genre are not suitable for kids at ${age} age`;
+    } else {
+      return `Those books are suitable`;
+    }
+  },
+
+  isItAffordable(price, budget) {
+    if (typeof price !== "number" || typeof budget !== "number") {
+      throw new Error("Invalid input");
+    }
+
+    let result = budget - price;
+
+    if (result < 0) {
+      return "You don't have enough money";
+    } else {
+      return `Book bought. You have ${result}$ left`;
+    }
+  },
+  
+  suitableTitles(array, wantedGenre) {
+    let resultArr = [];
+
+    if (!Array.isArray(array) || typeof wantedGenre !== "string") {
+      throw new Error("Invalid input");
+    }
+    array.map((obj) => {
+      if (obj.genre === wantedGenre) {
+        resultArr.push(obj.title);
+      }
+    });
+    return resultArr;
+  },
+};
+
+module.exports = { bookSelection }
