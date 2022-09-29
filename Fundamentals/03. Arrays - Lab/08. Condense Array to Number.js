f5e4ebb8d634ce6857c7b186d8ad338c44f32642
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-09-29 19:13:09 +0300

    Update 08. Condense Array to Number.js

--- Fundamentals/03. Arrays - Lab/08. Condense Array to Number.js
+++ Fundamentals/03. Arrays - Lab/08. Condense Array to Number.js
@@ -2,14 +2,11 @@ function condenseArrayToNumber(array){
 
         while (array.length > 1){
 
-            let condenseArray = [];
             for (let i = 0; i < array.length - 1; i++){
-            condenseArray[i] = array[i] + array[i+1]
-            array[i] = condenseArray[i]
+            array[i] = array[i] + array[i + 1]
             }
             array.length--
         }
-
     console.log(array[0])
 }
 condenseArrayToNumber([2,10,3])
