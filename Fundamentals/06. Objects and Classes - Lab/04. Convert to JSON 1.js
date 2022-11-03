aca594566f00e4efa4a9969b2bfba7578ee83af0
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-11-03 11:12:03 +0200

    Create 04. Convert to JSON 1.js

---
+++ Fundamentals/06. Objects and Classes - Lab/04. Convert to JSON 1.js
@@ -0,0 +1,9 @@
+function convertToJSON(name, lastName, hairColor) {
+    console.log(
+        JSON.stringify({
+            name,
+            lastName,
+            hairColor,
+        })
+    );
+}
