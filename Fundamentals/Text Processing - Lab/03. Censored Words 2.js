01c45c5c92eb85e4e12353aaba53907521fe1b49
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-11-14 17:18:57 +0200

    Delete Fundamentals/Text Processing - Lab directory

--- Fundamentals/Text Processing - Lab/03. Censored Words 2.js
+++
@@ -1,8 +0,0 @@
-function censoredWords(text, word){
-
-    let tokens = text.split(word);                       // Делим изречението на масив, на елементи, там където е думата 
-    console.log(tokens.join('*'.repeat(word.length)));   // след това съединяваме по броя звездички на дължината на думата 
-
-}
-//censoredWords('A small sentence with some words','small')
-censoredWords('Find the hidden word', 'hidden')
