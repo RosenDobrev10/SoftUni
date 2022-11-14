93a0185e3ad447923248ab97cb171d321bbccfb5
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-11-14 17:21:43 +0200

    Delete Fundamentals/Basic Syntax, Conditional Statements and Loops - More Exercise directory

--- Fundamentals/Basic Syntax, Conditional Statements and Loops - More Exercise/04. Reverse String.js
+++
@@ -1,7 +0,0 @@
-function reverseString(string) {
-    let reverseString = string.split("").reverse().join("");
-    console.log(reverseString);
-}
-reverseString("Hello");
-reverseString("SoftUni");
-reverseString("1234");
