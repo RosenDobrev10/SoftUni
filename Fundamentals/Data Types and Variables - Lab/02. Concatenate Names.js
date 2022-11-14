3f62b4f6187ade8050f1aac03c7c609bda38f698
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-11-14 17:20:59 +0200

    Delete Fundamentals/Data Types and Variables - Lab directory

--- Fundamentals/Data Types and Variables - Lab/02. Concatenate Names.js
+++
@@ -1,7 +0,0 @@
-function concatenateNames(firstName, lastName, delimeter) {
-    console.log(firstName + delimeter + lastName); // Конкатениране на стрингове чрез +
-    console.log(`${firstName}${delimeter}${lastName}`); // Интерполация на стрингове чрез ``
-}
-concatenateNames("John", "Smith", "->");
-concatenateNames("Jan", "White", "<->");
-concatenateNames("Linda", "Terry", "=>");
--- Fundamentals/Data Types and Variables - Lab/06. Gramophone.js
+++
@@ -1,8 +0,0 @@
-function gramophone(band, album, song) {
-    let songDuration = (album.length * band.length * song.length) / 2;
-    let rotations = Math.ceil(songDuration / 2.5);
-
-    console.log(`The plate was rotated ${rotations} times.`);
-}
-gramophone("Black Sabbath", "Paranoid", "War Pigs");
-gramophone("Rammstein", "Sehnsucht", "Engel");
