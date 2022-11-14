dfaea572bb732408f6bf3d51a64adcad8e5622ed
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-11-14 17:20:38 +0200

    Delete Fundamentals/Data Types and Variables - More Exercise directory

--- Fundamentals/Data Types and Variables - More Exercise/07. Triangle Area.js
+++
@@ -1,8 +0,0 @@
-function triangleArea(a, b, c) {
-    let p = (a + b + c) / 2;
-    let area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-    console.log(area);
-}
-triangleArea(2, 3.5, 4);
-triangleArea(3, 5.5, 4);
-// · Use Heron’s formula to obtain the result.
