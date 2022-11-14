7a32df3ecba18516c7141d8e6187d711d07a3781
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-11-14 17:21:08 +0200

    Delete Fundamentals/Data Types and Variables - Exercise directory

--- Fundamentals/Data Types and Variables - Exercise/04. Convert Meters to Kilometres.js
+++
@@ -1,6 +0,0 @@
-function convertMetersToKilometers(distanceInMeters) {
-    let kilometers = distanceInMeters / 1000; // МЕТРИТЕ СЕ ПРЕВРЪЩАТ В КИЛОМЕТРИ КАТО СЕ ДЕЛЯТ НА 1000 
-    console.log(kilometers.toFixed(2));                               
-}
-convertMetersToKilometers(1852);
-convertMetersToKilometers(798);
