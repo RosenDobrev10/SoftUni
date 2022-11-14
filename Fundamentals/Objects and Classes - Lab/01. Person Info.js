c15d538c0a5e2d5958f92482ee088d5c31e1f638
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-11-14 17:19:57 +0200

    Delete Fundamentals/Objects and Classes - Lab directory

--- Fundamentals/Objects and Classes - Lab/01. Person Info.js
+++
@@ -1,8 +0,0 @@
-function personInfo(firstName, lastName, age) {
-
-    let person = { firstName: firstName, lastName: lastName, age: age };  
-    // Декларираме обект и в него попълваме Key:Value  
-    return person;
-}
-console.log(personInfo("Peter", "Pan", 20));
-console.log(personInfo("George", "Smith", 18));
