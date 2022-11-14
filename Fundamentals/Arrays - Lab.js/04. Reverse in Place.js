9eb7239a65996ba90bf37438ef2f292bab97b515
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-11-14 17:13:28 +0200

    Delete Fundamentals/Arrays - Lab.js directory

--- Fundamentals/Arrays - Lab.js/04. Reverse in Place.js
+++
@@ -1,6 +0,0 @@
-function reverseInPlace(array) {
-    console.log(array.reverse().join(" "));
-}
-reverseInPlace(["a", "b", "c", "d", "e"]);
-reverseInPlace(["abc", "def", "hig", "klm", "nop"]);
-reverseInPlace(["33", "123", "0", "dd"]);
