93aaa10c1c798d9ae888882df027b558c2586546
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-01-11 20:24:30 +0200

    Create 07. Day of Week1.js

---
+++ JS Advanced/01. Syntax, Functions and Statements - Lab/07. Day of Week1.js
@@ -0,0 +1,12 @@
+function dayOfWeek(day) {
+    const days = {
+        'Monday': 1,
+        'Tuesday': 2,
+        'Wednesday': 3,
+        'Thursday': 4,
+        'Friday': 5,
+        'Saturday': 6,
+        'Sunday': 7,
+    };
+    days[day] ? console.log(days[day]) : console.log("error");
+}
