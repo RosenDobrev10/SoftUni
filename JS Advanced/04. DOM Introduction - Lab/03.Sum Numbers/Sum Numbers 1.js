edb247691a669688928628ee7001c41dc2e38ff1
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-01-13 08:58:13 +0200

    Create Sum Numbers 1.js

---
+++ JS Advanced/04. DOM Introduction - Lab/03.Sum Numbers/Sum Numbers 1.js
@@ -0,0 +1,6 @@
+function sumNumbers() {
+    const num1 = document.getElementById("num1");
+    const num2 = document.getElementById("num2");
+    const sum = document.getElementById("sum");
+    sum.value = Number(num1.value) + Number(num2.value);
+}
