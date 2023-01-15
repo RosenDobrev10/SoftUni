622a098cc6a4cda351605b156c97ee68364a152f
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-01-15 09:20:52 +0200

    Create 01. Person1.js

---
+++ JS Advanced/08. Classes and Attributes - Lab/01. Person1.js
@@ -0,0 +1,11 @@
+class Person {
+    constructor(firstName, lastName, age, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+    }
+    toString() {
+        return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
+    }
+}
