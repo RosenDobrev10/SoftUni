f2578f322ebd0a0f30355a43aa847258f73212b2
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-01-15 09:21:54 +0200

    Create 03. Circle1.js

---
+++ JS Advanced/08. Classes and Attributes - Lab/03. Circle1.js
@@ -0,0 +1,14 @@
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+    get diameter() {
+        return this.radius * 2;
+    }
+    set diameter(value) {
+        this.radius = value / 2;
+    }
+    get area() {
+        return Math.PI * this.radius ** 2;
+    }
+}
