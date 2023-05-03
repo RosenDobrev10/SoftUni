725c2fc067994f4767e7ac891d75909ebb527e26
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-05-03 08:18:57 +0300

    Create edit.js

---
+++ JS Applications/JS Applications Exam - 8 Aug 2021/src/views/edit.js
@@ -0,0 +1,85 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+import * as service from '../api/data.js';
+import { createSubmitHandler } from '../util.js';
+
+// Add @submit=${onSubmit} to form element and change variable name
+const editTemplate = (book, onSubmit) => html`<section
+	id="edit-page"
+	class="edit"
+>
+	<form @submit=${onSubmit} id="edit-form" action="#" method="">
+		<fieldset>
+			<legend>Edit my Book</legend>
+			<p class="field">
+				<label for="title">Title</label>
+				<span class="input">
+					<input
+						type="text"
+						name="title"
+						id="title"
+						.value=${book.title}
+					/>
+				</span>
+			</p>
+			<p class="field">
+				<label for="description">Description</label>
+				<span class="input">
+					<textarea
+						name="description"
+						id="description"
+						.value=${book.description}
+					></textarea>
+				</span>
+			</p>
+			<p class="field">
+				<label for="image">Image</label>
+				<span class="input">
+					<input
+						type="text"
+						name="imageUrl"
+						id="image"
+						.value=${book.imageUrl}
+					/>
+				</span>
+			</p>
+			<p class="field">
+				<label for="type">Type</label>
+				<span class="input">
+					<select id="type" name="type" .value=${book.type}>
+						<option value="Fiction" selected>Fiction</option>
+						<option value="Romance">Romance</option>
+						<option value="Mistery">Mistery</option>
+						<option value="Classic">Clasic</option>
+						<option value="Other">Other</option>
+					</select>
+				</span>
+			</p>
+			<input class="button submit" type="submit" value="Save" />
+		</fieldset>
+	</form>
+</section>`;
+
+export async function editPage(ctx) {
+	const id = ctx.params.id;
+	const book = await service.getById(id); // Change function name from service and variable name
+
+	ctx.render(editTemplate(book, createSubmitHandler(ctx, onSubmit))); // Change variable name
+}
+
+async function onSubmit(ctx, data, event) {
+	const id = ctx.params.id;
+	// data is object, Object.values(data) return array and we check with "some" if one field is empty it returns true
+	if (Object.values(data).some((value) => value === '')) {
+		return alert('All fields are required!');
+	}
+
+	await service.editById(id, {
+		title: data.title, // PROPERTY NAMES and VALUES WILL BE DIFFERENT !!! Take it from HTML
+		description: data.description,
+		imageUrl: data.imageUrl,
+		type: data.type,
+	});
+
+	event.target.reset();
+	ctx.page.redirect('/details/' + id);
+}
