a9995788137c12ddb03848ffca0ee6365c3ebf0c
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-05-04 07:36:57 +0300

    Create app.js

---
+++ JS Applications/JS Applications Exam - 8 Aug 2021/src/app.js
@@ -0,0 +1,33 @@
+import page from '../node_modules/page/page.mjs';
+
+import { addSession } from './middlewares/session.js';
+import { addRender } from './middlewares/render.js';
+
+import { homePage } from './views/home.js';
+import { loginPage } from './views/login.js';
+import { registerPage } from './views/register.js';
+import { createPage } from './views/create.js';
+import { detailsPage } from './views/details.js';
+import { editPage } from './views/edit.js';
+import { profilePage } from './views/profile.js';
+
+import { logout } from './api/user.js';
+
+page(addSession);
+page(addRender);
+
+page('/', homePage);
+page('/login', loginPage);
+page('/register', registerPage);
+page('/create', createPage);
+page('/details/:id', detailsPage);
+page('/edit/:id', editPage);
+page('/profile', profilePage);
+page('/logout', onLogout);
+
+page.start();
+
+function onLogout(ctx) {
+	logout();
+	ctx.page.redirect('/');
+}
