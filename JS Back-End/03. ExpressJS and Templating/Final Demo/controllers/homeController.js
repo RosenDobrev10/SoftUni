ffec279f6234ffe94b83cce9cab6dbcda834b40b
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-06-19 10:18:35 +0300

    Create homeController.js

---
+++ JS Back-End/03. ExpressJS and Templating/Final Demo/controllers/homeController.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    res.render('home');
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
+});
+
+module.exports = router;
