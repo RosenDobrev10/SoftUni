666a9d5a2d5d7a5813202cc2b981f5482a7b7ebb
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-06-29 07:46:03 +0300

    Create User.js

---
+++ JS Back-End/JS Back-End Exam - 01 November 2020/models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	hashedPassword: {
+		type: String,
+		required: true,
+	},
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
