68b1a7aad79b03aa0724ce0b97521f3198400a18
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-07-04 12:31:28 +0300

    Create session.js

---
+++ JS Back-End/JS Back-End Exam - 19 April 2022/middlewares/session.js
@@ -0,0 +1,19 @@
+const { verifyToken } = require("../services/userService.js");
+
+module.exports = () => (req, res, next) => {
+	const token = req.cookies.token;
+
+	if (token) {
+		try {
+            const userData = verifyToken(token);
+            req.user = userData;
+            res.locals.email = userData.email;
+            res.locals.firstName = userData.firstName;
+            res.locals.lastName = userData.lastName;
+		} catch (error) {
+			res.clearCookie('token');
+			return res.redirect('/auth/login');
+		}
+	}
+	next();
+};
