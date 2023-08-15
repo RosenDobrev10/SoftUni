6f652e4f53e039dc108bf65c4542f14973da4574
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-08-15 20:12:53 +0300

    Create express.js

---
+++ JS Back-End/JS Back-End Regular Exam - 18 June 2023/config/express.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+
+const cookieParser = require('cookie-parser');
+const session = require('../middlewares/session.js');
+const trimBody = require('../middlewares/trimBody.js');
+
+module.exports = (app) => {
+	app.engine('hbs', hbs.engine({extname: 'hbs'}));
+	app.set('view engine', 'hbs');
+
+	app.use('/static', express.static('static'));
+	app.use(express.urlencoded({ extended: true }));
+	app.use(cookieParser());
+	app.use(session());
+	app.use(trimBody());
+};
