aef6472f182a65c1cc9a2787f27434c28ab61896
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-08-17 19:32:36 +0300

    Create guards.js

---
+++ JS Back-End/JS Back-End Regular Exam - 18 June 2023/middlewares/guards.js
@@ -0,0 +1,24 @@
+function hasUser() {
+	return (req, res, next) => {
+		if (req.user) {
+			next();
+		} else {
+			res.redirect('/auth/login');	
+		}
+	};
+}
+
+function isGuest() {
+	return (req, res, next) => {
+		if (req.user) {
+			res.redirect('/');  
+		} else {
+			next();
+		}
+	};
+}
+
+module.exports = {
+    hasUser,
+    isGuest,
+};
