fc85e79572834bb9bc15ee7959880583cde2d31e
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-09-16 01:04:44 +0300

    Create authController.js

---
+++ JS Back-End/JS Back-End Retake Exam - 18 Aug 2022/controllers/authController.js
@@ -0,0 +1,71 @@
+const { isGuest, isLogged } = require('../middlewares/guards.js');
+const { register, login } = require('../services/userService.js');
+const { parseError } = require('../util/parser.js');
+
+const authController = require('express').Router();
+
+/////////////////////////////////////////// REGISTER ///////////////////////////////////////////
+authController.get('/register', isGuest(), (req, res) => {
+	res.render('register', {
+		title: 'Register Page',
+	});
+});
+
+authController.post('/register', isGuest(), async (req, res) => {
+	const { username, email, password, repass } = req.body;
+
+	try {
+        if (username == '' || email == '' || password == '') {
+            throw new Error('All fields are required');
+        }
+    
+        if (password != repass) {
+            throw new Error("Passwords don't match");
+        }
+
+		const token = await register(username, email, password);
+		res.cookie('token', token, { httpOnly: true });
+		res.redirect('/');
+	} catch (error) {
+		res.render('register', {
+			title: 'Register Page',
+			errors: parseError(error),
+			body: req.body
+		});
+	}
+});
+
+///////////////////////////////////////// LOGIN ///////////////////////////////////////////////
+authController.get('/login', isGuest(), (req, res) => {
+	res.render('login', {
+		title: 'Login Page',
+	});
+});
+
+authController.post('/login', isGuest(), async (req, res) => {
+	const { email, password } = req.body;
+
+	try {
+        if (email == '' || password == '') {
+            throw new Error('All fields are required');
+        }
+
+		const token = await login(email, password);
+		res.cookie('token', token, { httpOnly: true });
+		res.redirect('/');
+	} catch (error) {
+		res.render('login', {
+			title: 'Login Page',
+			errors: parseError(error),
+			body: req.body
+		});
+	}
+});
+
+///////////////////////////////////////// LOGOUT ///////////////////////////////////////////////
+authController.get('/logout', isLogged(), (req, res) => {
+	res.clearCookie('token');
+	res.redirect('/');
+});
+
+module.exports = authController;
