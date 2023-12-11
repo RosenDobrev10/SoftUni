d7837b8c21256b63f625b5e5e254a5a5475935c1
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2023-12-11 17:39:55 +0200

    delete project

--- Angular/Football-Players/src/app/core/environments/constants.ts
+++
@@ -1,5 +0,0 @@
-export const CONSTANTS = {
-  host: 'http://localhost:3030/',
-  // host: 'https://football-players.onrender.com/',
-  userData: 'userData',
-};
--- Angular/Football-Players/src/app/core/interfaces/Player.ts
+++
@@ -1,9 +0,0 @@
-export interface IPlayer {
-  _id: string;
-  name: string;
-  image: string;
-  age: string;
-  position: string;
-  club: string;
-  _ownerId: string;
-}
